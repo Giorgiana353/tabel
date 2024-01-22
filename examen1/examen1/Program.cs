7bb12d046f893cf6c5e7303ac89274e540bde4e0
Giorgiana353 <giorgiana.dinu@emanuel.ro>
2024-01-22 09:12:35 +0200

    ex

---
+++ examen1/examen1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace examen1
+{
+    public class Persoana
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int YearOfBirth { get; set; }
+
+        public int Age => DateTime.Now.Year - YearOfBirth;
+
+        public Persoana(string firstName, string lastName, int yearOfBirth)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            YearOfBirth = yearOfBirth;
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            Persoana persoanaEx = new Persoana("Daniela", "Giorgiana", 2004);
+
+            Console.WriteLine($"Nume si prenume: {persoanaEx.FirstName} {persoanaEx.LastName}");
+            Console.WriteLine($"Anul nasterii: {persoanaEx.YearOfBirth}");
+            Console.WriteLine($"Varsta: {persoanaEx.Age} ani");
+        }
+    }
+}
